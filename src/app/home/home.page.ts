5076132f79b9f18186d60a1dda14d9ce5bc5288f
incubox <be.salasm@duocuc.cl>
2023-12-07 18:31:26 -0300

    Examen

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  isMobile: boolean;
 
-  constructor() {}
+  constructor(private platform: Platform, private sanitizer: DomSanitizer) {
+    this.isMobile = this.platform.is('mobile');
+  }
 
+  descargarApk() {
+    // URL de GitHub Pages para tu archivo APK
+    const apkUrl = 'kitsune12333.github.io/apkduoc/app-release.apk';
+  
+    // Crea un enlace de descarga y haz clic en él programáticamente
+    const link = document.createElement('a');
+    link.href = apkUrl;
+    link.download = 'app-release.apk';
+    link.target = '_blank'; // Abrir en una nueva pestaña
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 }
